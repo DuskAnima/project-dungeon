36d5c9cbaf180513067280c61157b5751d86415f
Anima <cjaccardh@gmail.com>
2025-11-11 12:26:22 -0300

    EntityTerrainBus created. Now can emit signals between entity and terrain. Terrain now gets notified when entity complete its movement

--- scripts/terrain.gd
+++ scripts/terrain.gd
@@ -21,17 +21,17 @@ var ice_floor : TileData = tile_atlas.get_tile_data(Vector2i(2,0), 0)
 	if tile_data.get_custom_data_by_layer_id(ICE_FLOOR):
 		print("hielo")"""
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GridManager.set_terrain(self)
+	GridManager.set_terrain(self) # Entrega terreno para crear el grid
+	# Conecta el bus a la función de notificación de movimiento finalizado
 	EntityTerrainBus.connect_bus_to_signal_clean(_on_entity_movement_finished)
-
-func _get_entity_tile_data() -> TileData:
-	# Luego de reposicionarse, se actualiza la información del tile
-	entity_position = GridManager.get_entity_position() 
-	return self.get_cell_tile_data(entity_position) # Retorna la información del tile
 
 # Recibe una notificación de entity.gd a través del BUS sobre la consolidación del movimiento del entity.
 ## Con esto se evitan checks duplicados o innecesarios. 
 func _on_entity_movement_finished() -> void:
 	_get_entity_tile_data()
+
+func _get_entity_tile_data() -> TileData:
+	# Luego de reposicionarse, se actualiza la información del tile
+	entity_position = GridManager.get_entity_position() 
+	return self.get_cell_tile_data(entity_position) # Retorna la información del tile
